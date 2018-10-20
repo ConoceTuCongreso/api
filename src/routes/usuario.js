623a61721b06e2309013ca5e331131a2f8f9f563
Alejandro Güereca <ale_guereca_valdivia@hotmail.com>
2018-10-20 18:05:18 -0500

    Changed fetching of the data for login and added utils folder

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,36 +1,30 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const client = require('../database');
+const pool = require('../utils/database');
 
 const saltRounds = 10;
 const router = express.Router();
 
 router.post('/login', (req, res) => {
-  client.connect();
-  const query = {
-    name: 'fetch-user',
-    text: 'SELECT password_hash FROM users WHERE username = $1',
-    values: [req.body.username],
-  };
-  client.query(query)
-    .then((resq) => {
-      bcrypt.compare(req.body.password, resq.rows[0].password_hash).then((resb) => {
-        if (resb) {
-          res.sendStatus(200);
-        }
-      })
-        .catch((error) => {
-          console.log(error);
-          res.sendStatus(500);
-        });
-    })
-    .then(() => {
-      client.end();
-    })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(500);
-    });
+  (async () => {
+    const client = await pool.connect();
+    try {
+      const resq = await client.query('SELECT password_hash FROM users WHERE username = $1', [req.body.username])
+      const match = await bcrypt.compare(req.body.password, resq.rows[0].password_hash);
+      if (match) {
+        req.session.user = {
+          username: req.body.username,
+          email: req.body.email,
+          password: resq.rows[0].password_hash,
+        };
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(400);
+      }
+    } finally {
+      client.release();
+    }
+  })().catch(() => res.sendStatus(500));
 });
 
 router.post('/logout', (req, res) => {
@@ -51,7 +45,7 @@ router.post('/registro', (req, res) => {
       if (errh) {
         res.sendStatus(500);
       }
-      client.connect();
+      pool.connect();
       const query = {
         text: 'INSERT INTO users(first_name, user_id, username, middle_name, last_name, email, password_salt, password_hash) VALUES($1, (SELECT uuid_generate_v1()), $2, $3, $4, $5, $6, $7)',
         values: [req.body.first_name,
@@ -62,11 +56,11 @@ router.post('/registro', (req, res) => {
           salt,
           hash],
       };
-      client.query(query, (errq) => {
+      pool.query(query, (errq) => {
         if (errq) {
           res.sendStatus(500);
         } else {
-          client.end();
+          pool.end();
           req.session.user = {
             username: req.body.username,
             email: req.body.email,
