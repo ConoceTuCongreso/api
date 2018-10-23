72cdb3b7401caea005b46423aef90b0c3d278a38
Alejandro Güereca <ale_guereca_valdivia@hotmail.com>
2018-10-23 17:01:46 -0500

    Fixed database problem

--- src/utils/database.js
+++ src/utils/database.js
@@ -4,13 +4,8 @@ const fs = require('fs');
 const config = {
   user: process.env.USER_POSTGRES || 'postgres',
   database: process.env.DATABASE_POSTGRES,
-<<<<<<< HEAD
   host: process.env.HOST_POSTGRES || 'localhost',
   port: process.env.PORT_POSTGRES || 5432,
-=======
-  host: 'localHost',
-  port: process.env.PORT_POSTGRES,
->>>>>>> Added rough implementation of logging through winston
   password: process.env.PASSWORD_POSTGRES,
   ssl: {
     rejectUnauthorized: false,
