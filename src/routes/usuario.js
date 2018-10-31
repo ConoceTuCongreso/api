236b2672e2965f558329c8e53ec4a724d724a0f0
Miguel Miranda <mikemp96@gmail.com>
2018-10-31 17:40:51 -0600

    Make controller a class

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,7 +1,29 @@
 const express = require('express');
-const userController = require('../controllers/userController');
+const Logger = require('../utils/logger');
+const LoginValidator = require('../validations/input/loginValidation');
+const SignupValidator = require('../validations/input/signupValidation');
+const LoginDBValidation = require('../validations/database/loginDBValidation');
+const SignupDBValidation = require('../validations/database/signupDBValidation');
+const UserService = require('../services/usersService');
+const UserController = require('../controllers/user');
 
 const router = express.Router();
+
+// Init validators and services
+const logger = new Logger();
+const loginValidator = new LoginValidator();
+const signupValidator = new SignupValidator();
+const loginDBValidation = new LoginDBValidation();
+const signupDBValidation = new SignupDBValidation();
+const userService = new UserService();
+const userController = new UserController(
+  logger,
+  loginValidator,
+  signupValidator,
+  loginDBValidation,
+  signupDBValidation,
+  userService,
+);
 
 router.post('/login', userController.login);
 
