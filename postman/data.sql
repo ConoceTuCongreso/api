717676bcf9e170a33fdef07787c1cb84a4f64091
Miguel Miranda <mikemp96@gmail.com>
2018-11-14 19:50:49 -0600

    Update SQL

--- postman/data.sql
+++ postman/data.sql
@@ -18,3 +18,34 @@ INSERT INTO users(
     '$2b$10$/TkUIa9Toa2vDtwHiw9LL.',
     '$2b$10$/TkUIa9Toa2vDtwHiw9LL.3s6ooPnGTDRQvyk1/Hl0elG4Opw.zxC'
 );
+
+INSERT INTO categories(id, name) VALUES (1, 'Cultura'), (2, 'Reglamentos');
+
+INSERT INTO initiatives(
+    id,
+    description,
+    status_id,
+    document_url,
+    author,
+    infolej_number,
+    agreement_number
+)
+VALUES
+(
+    1,
+    'Iniciativa número 1',
+    1,
+    'http://congresoweb.congresojal.gob.mx',
+    'José Guadalupe Posada',
+    '001',
+    '001'
+),
+(
+    2,
+    'Iniciativa número 2',
+    1,
+    'http://congresoweb.congresojal.gob.mx',
+    'José Arango Arámbula',
+    '002',
+    '002'
+);
