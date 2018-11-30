13026f3289f91d3e39a4c0433cb7ac9c9ab75f72
Alejandro Güereca <ale_guereca_valdivia@hotmail.com>
2018-11-29 20:16:09 -0600

    Add max age for cookies

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    expires: 600000,
+    maxAge: process.env.COOKIE_MAX_AGE,
   },
 }));
 app.use((req, res, next) => {
